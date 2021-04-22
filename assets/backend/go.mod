12cf2a4836e2969d3c143aff6f947e16c0ca9a02
BBAllieP <brabrah@protonmail.com>
2021-04-22 16:29:41 +0200

    maintaining mission csv now

--- assets/backend/go.mod
+++ assets/backend/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/iancoleman/strcase v0.1.3 // indirect
+	github.com/mohae/struct2csv v0.0.0-20151122200941-e72239694eae // indirect
 )
--- assets/backend/utils.go
+++ assets/backend/utils.go
@@ -1,25 +1,32 @@
 package main
 
+import (
+	"bytes"
+	"io/ioutil"
+
+	"github.com/mohae/struct2csv"
+)
+
 func Find(slice []string, val string) (int, bool) {
-    for i, item := range slice {
-        if item == val {
-            return i, true
-        }
-    }
-    return -1, false
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
 }
 
-func bucketFactions(missions *[]Mission) []Faction{
+func bucketFactions(missions *[]Mission) []Faction {
 	found := false
 	var activeFactionList []Faction
-	for _, mission := range(*missions) {
-		for _, fact := range(activeFactionList){
+	for _, mission := range *missions {
+		for _, fact := range activeFactionList {
 			found = true
 			if fact.Name == mission.TargetFaction {
 				fact.Missions = append(fact.Missions, mission)
 			}
 		}
-		if !found{
+		if !found {
 			activeFactionList = append(activeFactionList, Faction{[]Mission{mission}, mission.TargetFaction})
 		}
 		found = false
@@ -27,12 +34,22 @@ func bucketFactions(missions *[]Mission) []Faction{
 	return activeFactionList
 }
 
-func factionMissionsGetter(missions *[]Mission, bountyTarget string) []Mission{
+func factionMissionsGetter(missions *[]Mission, bountyTarget string) []Mission {
 	var missionList []Mission
-	for _, mission := range(*missions) {
-		if mission.TargetFaction == bountyTarget && mission.Status == "Progress"{
+	for _, mission := range *missions {
+		if mission.TargetFaction == bountyTarget && mission.Status == "Progress" {
 			missionList = append(missionList, mission)
 		}
 	}
 	return missionList
+}
+
+func writeCsv(missions *[]Mission) {
+	buff := &bytes.Buffer{}
+	w := struct2csv.NewWriter(buff)
+	err := w.WriteStructs(*missions)
+	if err != nil {
+		// handle error
+	}
+	ioutil.WriteFile("output.csv", buff.Bytes(), 0644)
 }
