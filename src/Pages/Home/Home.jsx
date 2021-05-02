b21a7eaeb0a0c2bb62e5956ceef4ebd15cb1f023
Allie Piemont <brabrah@protonmail.com>
2021-05-02 12:11:53 +0200

    fixed homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { Typography, Grid } from "@material-ui/core";
+import { Typography, Grid, Divider, Link } from "@material-ui/core";
+import logo from "../../assets/images/EDToolsAppIconTransparent.png";
+
 const Home = () => (
-	<Grid container spacing={3}>
+	<Grid container spacing={3} direction='column' justify='space-evenly'>
 		<Grid item xs={12}>
-			<Typography variant='h1' component='h2' gutterBottom>
+			<Typography variant='h2' component='h2' gutterBottom>
 				Welcome to CMDR CPT Allie's Elite Dangerous Tools
 			</Typography>
+		</Grid>
+		<Grid item xs={12}>
+			<img
+				src={logo}
+				style={{
+					marginTop: 20,
+					height: "50vh",
+				}}
+			/>
+		</Grid>
+		<Grid item container xs={12} spacing={3}>
+			<Grid item xs={12}>
+				<Divider />
+			</Grid>
+			<Grid item xs={12} container justify='flex-end' spacing={2}>
+				<Typography variant='button'>
+					<Link href='https://ko-fi.com/bballiep' target='_blank'>
+						Buy me a beer
+					</Link>
+				</Typography>
+			</Grid>
 		</Grid>
 	</Grid>
 );
