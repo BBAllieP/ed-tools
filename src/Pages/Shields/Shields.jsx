84285316e43ad0aa202ad21c5046d86498482ff2
BBAllieP <brabrah@protonmail.com>
2021-05-29 11:52:15 +0200

    fix syntax error on route planner

--- src/Pages/Shields/Shields.jsx
+++ src/Pages/Shields/Shields.jsx
@@ -5,7 +5,7 @@ const Shields = () => (
     <Grid container spacing={3} direction='column' justify='space-evenly'>
         <Grid item xs={12}>
             <Typography variant='h2' component='h2' gutterBottom>
-                The Route Planner is Currently Under Construction
+                The Shield Calculator is Currently Under Construction
             </Typography>
         </Grid>
         <Grid item xs={12}>
