21fb86146af11b4e4b45b3c8be144c5c4fdb3bf5
BBAllieP <brabrah@protonmail.com>
2021-06-06 19:33:21 +0200

    build for link behavior fix

--- src/Pages/Home/ico.js
+++ src/Pages/Home/ico.js
@@ -1,5 +1,6 @@
 import SvgIcon from '@material-ui/core/SvgIcon';
 import { ReactComponent as StationIcon } from '../../resources/img/Coriolis-Station.svg';
+import { ReactComponent as BeerIcon } from '../../resources/img/pint-of-beer-svgrepo-com.svg';
 
 function HomeIcon(props) {
     return (
@@ -8,4 +9,12 @@ function HomeIcon(props) {
       </SvgIcon>
     );
 } 
+
+export function BeerIco(props){
+  return(
+    <SvgIcon {...props}>
+      <BeerIcon />
+    </SvgIcon>
+  )
+}
 export default HomeIcon;
