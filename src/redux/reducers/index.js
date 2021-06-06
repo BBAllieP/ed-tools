ff59c9eed67043d0a8768965ec9e7d04afae2fd2
BBAllieP <brabrah@protonmail.com>
2021-06-06 15:48:01 +0200

    started route planner

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import missions from "./missions";
 import ui from "./ui";
 import { websocketReducer } from "./websocket";
+import routes from "./routes";
 import { combineReducers } from "redux";
 
-export default combineReducers({ ui, missions, websocketReducer });
+export default combineReducers({ ui, missions, websocketReducer, routes });
