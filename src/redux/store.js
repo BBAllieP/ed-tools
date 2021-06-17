afe170ad50e7a25b04d579a5ae60ed07484311a1
BBAllieP <brabrah@protonmail.com>
2021-06-17 17:27:00 +0200

    build prep

--- src/redux/store.js
+++ src/redux/store.js
@@ -28,5 +28,5 @@ export default makeStore();*/
 export default createStore(rootReducer,
 	compose(
 		applyMiddleware(wsMiddleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	));
