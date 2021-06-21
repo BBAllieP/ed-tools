20bd20b6628536cac1c83df8361edc28be29ee64
BBAllieP <brabrah@protonmail.com>
2021-06-21 09:15:00 +0200

    prebuild script

--- assets/backend/routes.go
+++ assets/backend/routes.go
@@ -188,10 +188,9 @@ func DeleteRoute() {
 	CurrentRoute = Route{"", "", "", "", 0, nil}
 	loadFile := getStorageDir() + "\\route.json"
 	if _, err := os.Stat(loadFile); err == nil {
-		return
+		file, _ := json.MarshalIndent(CurrentRoute, "", " ")
+		_ = ioutil.WriteFile(getStorageDir()+"\\route.json", file, 0644)
 	}
-	file, _ := json.MarshalIndent(CurrentRoute, "", " ")
-	_ = ioutil.WriteFile(getStorageDir()+"\\route.json", file, 0644)
 }
 
 /*
